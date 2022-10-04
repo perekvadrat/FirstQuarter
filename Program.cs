630ebd68bbaddfd6f1e62bf1d39b6d30b32d8b47
perekvadrat <perekvadrat@gmail.com>
2022-10-04 21:52:11 +0800

    the main program is written

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿
+﻿string[] arr = { "hi", "hello", "geekbrains", "programmer", "^_^", "git" };
+
+void PrintArray(string[] array)
+{
+    int length = array.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void Choose3OrLess(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count += 1;
+    }
+
+    string[] newarr = new string[count];
+
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarr[j] = arr[i];
+            j++;
+        }
+    }
+    PrintArray(newarr);
+}
+
+PrintArray(arr);
+Console.Write("Новый массив: ");
+Choose3OrLess(arr);
 
